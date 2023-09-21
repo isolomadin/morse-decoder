78dc31fcaf4410bd5734d6ed30baeb02ff5fc691
Илья <isolomadin@MacBook-Pro-Ila.local>
2023-09-21 12:58:17 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let morseString = '';
+    let morseLetterLength = 0;
+    for(let i = 0; i < expr.length - 1; i+=2) {
+        let numberLetter = expr.slice(i, i + 2);
+        if (numberLetter === '10') morseString += '.';
+        if (numberLetter == '11') morseString += '-';
+        morseLetterLength += 2;
+        if (morseLetterLength === 10) {
+            morseLetterLength = 0;
+            morseString += ' ';
+        }
+    }
+
+    const morseStringSplit = morseString.split(" ");
+    let result = "";
+
+    for (let i = 0; i < morseStringSplit.length - 1; i++) {
+        let decodeWord = MORSE_TABLE[morseStringSplit[i]];
+
+        if (decodeWord) result += decodeWord;
+        else result += " ";
+    }
+
+    return result;
 }
 
 module.exports = {
